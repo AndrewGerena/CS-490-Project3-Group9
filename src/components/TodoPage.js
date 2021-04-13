899a7707546cb9a727ab34c9090c61332f676184
Andrew Gerena <amg46@njit.edu>
2021-04-13 18:41:24 +0000

    Added input text and submit button

---
+++ src/components/TodoPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export function TodoPage() {
+    return (
+        <div>
+            <form>
+                <input type="text" />
+                <input type="submit" value="Submit" />
+            </form>
+        </div>
+        )
+}
