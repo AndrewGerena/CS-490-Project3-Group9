d9046d9914211a3c20b5e8841c725019f70f6715
Sainithin Kuntamukkala <sk2266@njit.edu>
2021-04-14 18:14:01 +0000

    Implemented basic login auth functionality

--- src/login.js
+++ src/login.js
@@ -5,11 +5,33 @@ import { socket } from './App.js';
 import { GoogleLogin } from 'react-google-login'; 
 import os from 'os';
 
-const CLIENT_ID = os.getenv('CLIENT_ID')
-const CLIENT_SECRET = os.getenv('CLIENT_SECRET') 
+const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
 
 export function Login(props) {
-    
-}
+  const onSuccess = (res) => {
+    console.log('Login Success: currentUser:', res.profileObj);
+    alert(
+      `Login Successful. Welcome ${res.profileObj.name}.`
+    );
+  };
 
-export default Login;
+  const onFailure = (res) => {
+    console.log('Login failed: res:', res);
+  };
+
+  return (
+    <div>
+      <center>
+          <GoogleLogin
+            clientId={CLIENT_ID}
+            buttonText="Sign in with Google"
+            onSuccess={onSuccess}
+            onFailure={onFailure}
+            cookiePolicy={'single_host_origin'}
+            style={{ marginTop: '100px' }}
+            isSignedIn={false}
+          />
+          </center>
+    </div>
+  );
+}
