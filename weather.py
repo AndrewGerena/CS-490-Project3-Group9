eddc191f16971b5cc4d6a2c37dea9edf5a46d8f6
Amandeep Singh <40546763+SinghSanity@users.noreply.github.com>
2021-04-16 19:17:29 +0000

    Added a function to get weather icons from openweather. Added import to app.py

--- weather.py
+++ weather.py
@@ -2,13 +2,11 @@ import requests
 import os
 from dotenv import load_dotenv, find_dotenv
 from date import convert
+from icon import get_icon
 
 load_dotenv(find_dotenv())
 WEATHER_KEY = os.getenv('WEATHER_KEY')
 
-lat = "40.902924"
-lon = "-74.094005"
-# Dummy values, will be changed once we add in ZIP code to Lat/Lon conversion.
 
 def get_location(zipcode):
     '''Uses another OpenWeather API to get latitude and longitude'''
@@ -32,42 +30,54 @@ def get_weather(zipcode):
    
     current_temp = str(data["current"]["temp"])
     current_weather = str(data["current"]["weather"][0]["main"])
-    today = [town, current_temp, current_weather]
+    icon_curr = str(data["current"]["weather"][0]["icon"])
+    icon_curr = get_icon(icon_curr)
+    today = [town, current_temp, current_weather, icon_curr]
     
     dt_day_1 = str(data["daily"][0]["dt"])
     date_day_1, dow_1 = convert(dt_day_1)
     max_day_1 = str(data["daily"][0]["temp"]["max"])
     min_day_1 = str(data["daily"][0]["temp"]["min"])
     weather_day_1 = str(data["daily"][0]["weather"][0]["main"])
-    day_1 = [date_day_1, dow_1, max_day_1, min_day_1, weather_day_1]
+    icon_day_1 = str(data["daily"][0]["weather"][0]["icon"])
+    icon_day_1 = get_icon(icon_day_1)
+    day_1 = [date_day_1, dow_1, max_day_1, min_day_1, weather_day_1, icon_day_1]
     
     dt_day_2 = str(data["daily"][1]["dt"])
     date_day_2, dow_2 = convert(dt_day_2)
     max_day_2 = str(data["daily"][1]["temp"]["max"])
     min_day_2 = str(data["daily"][1]["temp"]["min"])
     weather_day_2 = str(data["daily"][1]["weather"][0]["main"])
-    day_2 = [date_day_2, dow_2, max_day_2, min_day_2, weather_day_2]
+    icon_day_2 = str(data["daily"][1]["weather"][0]["icon"])
+    icon_day_2 = get_icon(icon_day_2)
+    day_2 = [date_day_2, dow_2, max_day_2, min_day_2, weather_day_2, icon_day_2]
     
     dt_day_3 = str(data["daily"][2]["dt"])
     date_day_3, dow_3 = convert(dt_day_3)
     max_day_3 = str(data["daily"][2]["temp"]["max"])
     min_day_3 = str(data["daily"][2]["temp"]["min"])
     weather_day_3 = str(data["daily"][2]["weather"][0]["main"])
-    day_3 = [date_day_3, dow_3, max_day_3, min_day_3, weather_day_3]
+    icon_day_3 = str(data["daily"][2]["weather"][0]["icon"])
+    icon_day_3 = get_icon(icon_day_3)
+    day_3 = [date_day_3, dow_3, max_day_3, min_day_3, weather_day_3, icon_day_3]
     
     dt_day_4 = str(data["daily"][3]["dt"])
     date_day_4, dow_4 = convert(dt_day_4)
     max_day_4 = str(data["daily"][3]["temp"]["max"])
     min_day_4 = str(data["daily"][3]["temp"]["min"])
     weather_day_4 = str(data["daily"][3]["weather"][0]["main"])
-    day_4 = [date_day_4, dow_4, max_day_4, min_day_4, weather_day_4]
+    icon_day_4 = str(data["daily"][3]["weather"][0]["icon"])
+    icon_day_4 = get_icon(icon_day_4)
+    day_4 = [date_day_4, dow_4, max_day_4, min_day_4, weather_day_4, icon_day_4]
     
     dt_day_5 = str(data["daily"][4]["dt"])
     date_day_5, dow_5 = convert(dt_day_5)
     max_day_5 = str(data["daily"][4]["temp"]["max"])
     min_day_5 = str(data["daily"][4]["temp"]["min"])
     weather_day_5 = str(data["daily"][4]["weather"][0]["main"])
-    day_5 = [date_day_5, dow_5, max_day_5, min_day_5, weather_day_5]
+    icon_day_5 = str(data["daily"][4]["weather"][0]["icon"])
+    icon_day_5 = get_icon(icon_day_5)
+    day_5 = [date_day_5, dow_5, max_day_5, min_day_5, weather_day_5, icon_day_5]
     
     lst = [today, day_1, day_2, day_3, day_4, day_5]
     
@@ -75,16 +85,20 @@ def get_weather(zipcode):
 
 
 weather_data = get_weather("07663")
-#get_location("07663")
 
-print("Town: " + str(weather_data[0][0]) + " Current Temp: " + str(weather_data[0][1]) + " Current Weather: " + weather_data[0][2])
+print("Town: " + str(weather_data[0][0]) + " Current Temp: " + str(weather_data[0][1]) + " Current Weather: " + weather_data[0][2] + " ICON URL: " + weather_data[0][3])
     
 print("Date: " + str(weather_data[1][0]) + " " + str(weather_data[1][1]) + " HIGH: " + str(weather_data[1][2]) + " LOW: " + str(weather_data[1][3]) + " Weather: " + weather_data[1][4])
-    
+print("ICON URL: " + weather_data[1][5] + "\n")
+
 print("Date: " + str(weather_data[2][0]) + " " + str(weather_data[2][1]) + " HIGH: " + str(weather_data[2][2]) + " LOW: " + str(weather_data[2][3]) + " Weather: " + weather_data[2][4])
-    
+print("ICON: " + weather_data[2][5] + "\n")
+
 print("Date: " + str(weather_data[3][0]) + " " + str(weather_data[3][1]) + " HIGH: " + str(weather_data[3][2]) + " LOW: " + str(weather_data[3][3]) + " Weather: " + weather_data[3][4])
-    
+print("ICON: " + weather_data[3][5] + "\n")
+
 print("Date: " + str(weather_data[4][0]) + " " + str(weather_data[4][1]) + " HIGH: " + str(weather_data[4][2]) + " LOW: " + str(weather_data[4][3]) + " Weather: " + weather_data[4][4])
-    
+print("ICON: " + weather_data[4][5] + "\n")
+
 print("Date: " + str(weather_data[5][0]) + " " + str(weather_data[5][1]) + " HIGH: " + str(weather_data[5][2]) + " LOW: " + str(weather_data[5][3]) + " Weather: " + weather_data[5][4])
+print("ICON: " + weather_data[5][5] + "\n")
