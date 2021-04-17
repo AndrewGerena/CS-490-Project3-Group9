47a14b3d17df5abcafde9ece4bdc41144a7887c5
Amandeep Singh <40546763+SinghSanity@users.noreply.github.com>
2021-04-17 16:27:19 +0000

    Added input field for user to input their zipcode, NOT FUNCTIONAL YET

--- src/Profile.js
+++ src/Profile.js
@@ -3,14 +3,28 @@ import { socket } from './App.js';
 import './App.css';
 
 function Profile() {
-
+  const inputRef = useRef();
+  
+  function zipButton(){
+        if (inputRef.current.value !== '') {
+            var zipcode = inputRef.current.value;
+            alert(zipcode);
+        }
+    }
+  
   return (
     <div>
         <h1>Welcome to the Profile Page!</h1>
+        <div>
+        <h3>Set your zipcode here</h3>
+        <input ref={inputRef} type="text" aria-label="zipcode-input" />
+            <button onClick={zipButton} type="submit">
+                Confirm
+            </button>
+        </div>
     </div>
   );
 }
-
 
 
 export { Profile as default };
