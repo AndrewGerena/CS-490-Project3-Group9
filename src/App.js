42e1544dc995db64f9d06a62e418962e848ad0b3
Sunny Raval <Sjr52@njit.edu>
2021-04-17 00:49:12 +0000

    Fixed Merge problems

---
+++ src/App.js
@@ -0,0 +1,16 @@
+  
+import logo from './logo.svg';
+import './App.css';
+import { Login } from './login.js'; // render the Login component from login.js
+import io from 'socket.io-client';
+export const socket = io(); // Newly added this to App.js
+
+function App() {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
