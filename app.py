326eaaccf1a0307ab06a8c58f4692c9e9e5ebde3
Amandeep Singh <40546763+SinghSanity@users.noreply.github.com>
2021-04-17 18:51:29 +0000

    Created function to check valid zipcode, and linted some files

--- app.py
+++ app.py
@@ -7,8 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 #from sqlalchemy import desc
 from dotenv import load_dotenv, find_dotenv
 from weather import get_weather
-
-
+#from zip_check import check_zip # commented out for now
 
 load_dotenv(find_dotenv())
 
@@ -26,8 +25,8 @@ import models
 
 DB.create_all()
 
-cors = CORS(APP, resources={r"/*": {"origins": "*"}})
-socketio = SocketIO(APP,
+CORS = CORS(APP, resources={r"/*": {"origins": "*"}})
+SOCKETIO = SocketIO(APP,
                     cors_allowed_origins="*",
                     json=json,
                     manage_session=False)
@@ -43,21 +42,21 @@ def index(filename):
 
 
 # When a client connects from this Socket connection, this function is run
-@socketio.on('connect')
+@SOCKETIO.on('connect')
 def on_connect():
     """Function is accessed upon user connection"""
     print('User connected!')
 
 
 # When a client disconnects from this Socket connection, this function is run
-@socketio.on('disconnect')
+@SOCKETIO.on('disconnect')
 def on_disconnect():
     """Function is accessed upon user disconnection"""
     print('User disconnected!')
 
 
 # Login functionality
-@socketio.on('login')
+@SOCKETIO.on('login')
 def user_login(data):
     """User list is updated upon login events"""
     print(str(data))
@@ -70,22 +69,25 @@ def user_login(data):
             user_exists = True
             break
 
-    socketio.emit('login', {
+    SOCKETIO.emit('login', {
         'info': data,
         'user_exists': user_exists,
     },
                   broadcast=True,
                   include_self=True)  ## changing include self to true
 
-@socketio.on('forecast')
+
+@SOCKETIO.on('forecast')
 def on_forecast(data):
     '''Will fetch zipcode from DB and return local weather'''
-    data = get_weather("10001") # Default for now. Will update when we can fetch the zipcode.
-    socketio.emit('forecast', data, broadcast=False, include_self=True)
+    data = get_weather(
+        "10001")  # Default for now. Will update when we can fetch the zipcode.
+    SOCKETIO.emit('forecast', data, broadcast=False, include_self=True)
+
 
 # Note that we don't call app.run anymore. We call socketio.run with app arg
 if __name__ == "__main__":
-    socketio.run(
+    SOCKETIO.run(
         APP,
         host=os.getenv('IP', '0.0.0.0'),
         port=8081 if os.getenv('C9_PORT') else int(os.getenv('PORT', 8081)),
