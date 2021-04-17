753b31fdec55fb16b4c1eae313b0158cbb5f01aa
Amandeep Singh <40546763+SinghSanity@users.noreply.github.com>
2021-04-17 12:56:10 +0000

    Front end now relies on back end for weather requests

--- src/sample.js
+++ src/sample.js
@@ -12,16 +12,16 @@ export function Sample(props) {
     <div>
         <div className = "current">
             <h2>{forecast[0][0]}</h2>
-            <h3>Currently: 57°</h3>
-            <h3>Insert Weather Here</h3>
-            <img src="https://openweathermap.org/img/wn/10d@2x.png" alt="weather" />
+            <h3>Currently: {forecast[0][1]}°</h3>
+            <h3>{forecast[0][2]}</h3>
+            <img src={forecast[0][3]} alt="weather" />
         </div>
         <div className ="forecast">
-            <Tile day={"Monday"} date={"04/19"} high={"10"} low={"5"} weather="Cloudy" icon={"https://openweathermap.org/img/wn/10d@2x.png"}/>
-            <Tile day={"Monday"} date={"04/19"} high={"10"} low={"5"} weather="Cloudy" icon={"https://openweathermap.org/img/wn/10d@2x.png"}/>
-            <Tile day={"Monday"} date={"04/19"} high={"10"} low={"5"} weather="Cloudy" icon={"https://openweathermap.org/img/wn/10d@2x.png"}/>
-            <Tile day={"Monday"} date={"04/19"} high={"10"} low={"5"} weather="Cloudy" icon={"https://openweathermap.org/img/wn/10d@2x.png"}/>
-            <Tile day={"Monday"} date={"04/19"} high={"10"} low={"5"} weather="Cloudy" icon={"https://openweathermap.org/img/wn/10d@2x.png"}/>
+            <Tile day={forecast[1][1]} date={forecast[1][0]} high={forecast[1][2]} low={forecast[1][3]} weather={forecast[1][4]} icon={forecast[1][5]}/>
+            <Tile day={forecast[2][1]} date={forecast[2][0]} high={forecast[2][2]} low={forecast[2][3]} weather={forecast[2][4]} icon={forecast[2][5]}/>
+            <Tile day={forecast[3][1]} date={forecast[3][0]} high={forecast[3][2]} low={forecast[3][3]} weather={forecast[3][4]} icon={forecast[3][5]}/>
+            <Tile day={forecast[4][1]} date={forecast[4][0]} high={forecast[4][2]} low={forecast[4][3]} weather={forecast[4][4]} icon={forecast[4][5]}/>
+            <Tile day={forecast[5][1]} date={forecast[5][0]} high={forecast[5][2]} low={forecast[5][3]} weather={forecast[5][4]} icon={forecast[5][5]}/>
         </div>
         <br />
     </div>
