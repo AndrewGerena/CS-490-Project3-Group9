e34cde98c0e5dc8fcad1caf65838cee32a4c5786
Andrew Gerena <amg46@njit.edu>
2021-05-03 20:33:04 +0000

    Linted TodoList

--- src/ToDoComponents/TodoList.js
+++ src/ToDoComponents/TodoList.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import { Task } from './Task';
 
+export function TodoList({ tasks, toggle }) {
+  console.log(tasks);
 
+  return (
+    <div>
+      {tasks.map((task) => <Task key={task.id} task={task} toggle={toggle} className="TodoTasks" />)}
+    </div>
+  );
+}
 
-export function TodoList({tasks, toggle}) {
-    
-    console.log(tasks);
-    
-    return (
-        <div>
-            {tasks.map((task) => {
-                return <Task key={task.id} task={task} toggle={toggle} className="TodoTasks"/>;
-            })}
-        </div>
-        );
-} 
+export default TodoList;
