5d26e78465bd005c78f15dd94aee0036bdda0a86
Andrew Gerena <amg46@njit.edu>
2021-05-04 03:37:04 +0000

    Linted dashboard

--- src/dashboard.js
+++ src/dashboard.js
@@ -17,20 +17,21 @@ export function DashBoard(props) {
 
   const emailRef = useRef(null);
   emailRef.current = props.email;
-  
+
   const firstName = props.name;
   const profilePic = props.picURL;
-  console.log(props.name); 
-  
-  var date = new Date();
-  var month, day, year, hour, min, sec; 
-  month = date.getMonth()+1;
+  console.log(props.name);
+
+  const date = new Date();
+  let month; let day; let year; let hour; let min; let
+    sec;
+  month = date.getMonth() + 1;
   day = date.getDate();
   year = date.getFullYear();
   hour = date.getHours();
   min = date.getMinutes();
   sec = date.getSeconds();
-    
+
   function onClickProfile() {
     setProfile(true);
     setWeather(false);
@@ -72,72 +73,94 @@ export function DashBoard(props) {
   useEffect(() => {
     // When a move has been made.
     socket.on('forecast', (data) => {
-    if (data.email == emailRef.current){
-      setForecast(data.weather);
-      console.log(data.weather);
-    }
+      if (data.email == emailRef.current) {
+        setForecast(data.weather);
+        console.log(data.weather);
+      }
     });
   }, []);
-  var message = "";
+  let message = '';
   if (!weather && !news && !todo && !profile) {
-    message = <div className="welcome-msg">
-                  <div className="Main_Welcome_Div">
-                    <div className = "Home_Wrapper">
-                      <h1 className = "DashHome_h1">Hello {firstName}!</h1>
-                      <img className = "Fire_TL" src="https://res.cloudinary.com/ddsomtotk/image/upload/v1612802629/FireBurn-Pic-uvz5ud-unscreen_fvktbd.gif"/>
-                      <img className = "Fire_TR" src="https://res.cloudinary.com/ddsomtotk/image/upload/v1612802629/FireBurn-Pic-uvz5ud-unscreen_fvktbd.gif"/>
-                      <img className = "Fire_RB" src="https://res.cloudinary.com/ddsomtotk/image/upload/v1612802629/FireBurn-Pic-uvz5ud-unscreen_fvktbd.gif"/>
-                      <img className = "Fire_LB" src="https://res.cloudinary.com/ddsomtotk/image/upload/v1612802629/FireBurn-Pic-uvz5ud-unscreen_fvktbd.gif"/>
-                    </div>
-                    <div className = "Image_Wrapper glow">
-                      <img src={profilePic} />
-                    </div>
-                  </div>
-                  <div className = "Time_Div">
-                    <h3 className = "DashHome_h3">Thursday, {month}/{day}/{year}</h3>
-                    <div className = "Hours"><p>Hours</p><h1>{hour}</h1></div>
-                    <p className="Colon_1">:</p>
-                    <div className = "Mins"><p>Mins</p><h1>{min}</h1></div>
-                    <p className="Colon_2">:</p>
-                    <div className= "Secs"><p>Secs</p><h1>{sec}</h1></div>
-                  </div>
-              </div>;
-  }
-    
-    return (
-         <div>
-            <div className="header NewsPage_Header">
-  	            <div className="header-top"></div>
-  	            <div className="NavBar">
-  	                <a className="Company_Logo"><img src='https://res.cloudinary.com/ddsomtotk/image/upload/v1618887646/57dd63e9c36d40e8aa369502ee886d0e_lmpcru.png' alt="Comp_logo"/></a>
-                </div>
-                <div className="header-bottom"></div>
-                    <center>
-                        <div className="dash_card">
-                            <ul className = "dash_btn">
-                                <img  className="profile_img" src ="https://res.cloudinary.com/ddsomtotk/image/upload/v1618947076/Prof_img_kxovem.png" />
-                                <button data-testid="profile-btn" onClick={onClickProfile} className="prof-btn btn">Personal Profile</button>
-                                <img  className="weather_img" src ="https://res.cloudinary.com/ddsomtotk/image/upload/v1618946882/WeatherImg_d05jmk.png" />
-                                <button data-testid="weather-btn" onClick={onClickWeather} className="weath-btn btn">Local Weather</button>
-                                <img  className="news_img" src ="https://res.cloudinary.com/ddsomtotk/image/upload/v1618946888/Newsimg_harljy.png" />
-                                <button data-testid="news-btn" onClick={onClickNews} className="news-btn btn">News Updates</button>
-                                <img  className="todo_img" src ="https://res.cloudinary.com/ddsomtotk/image/upload/v1618946885/Todolist_su8dg0.png" />
-                                <button data-testid="todo-btn" onClick={onClickTodo} className="todo-btn btn">Daily Tasks</button>
-                            </ul>
-                        </div>
-                    </center>
-            </div>
-            {message}
-            <div className="Comp_Render">
-                {test}
-            </div>
-            <div className="footer NewsPage_Footer">
-      	        <div className="footer-top"></div>
-      	        <div className="footer-center NewsPage_FooterText"><p>&copy; SASA Inc. All Rights Reserved.</p></div>
-      	        <div className="footer-bottom"></div>
-            </div>
+    message = (
+      <div className="welcome-msg">
+        <div className="Main_Welcome_Div">
+          <div className="Home_Wrapper">
+            <h1 className="DashHome_h1">
+              Hello
+              {firstName}
+              !
+            </h1>
+            <img className="Fire_TL" src="https://res.cloudinary.com/ddsomtotk/image/upload/v1612802629/FireBurn-Pic-uvz5ud-unscreen_fvktbd.gif" />
+            <img className="Fire_TR" src="https://res.cloudinary.com/ddsomtotk/image/upload/v1612802629/FireBurn-Pic-uvz5ud-unscreen_fvktbd.gif" />
+            <img className="Fire_RB" src="https://res.cloudinary.com/ddsomtotk/image/upload/v1612802629/FireBurn-Pic-uvz5ud-unscreen_fvktbd.gif" />
+            <img className="Fire_LB" src="https://res.cloudinary.com/ddsomtotk/image/upload/v1612802629/FireBurn-Pic-uvz5ud-unscreen_fvktbd.gif" />
+          </div>
+          <div className="Image_Wrapper glow">
+            <img src={profilePic} />
+          </div>
         </div>
+        <div className="Time_Div">
+          <h3 className="DashHome_h3">
+            Thursday,
+            {month}
+            /
+            {day}
+            /
+            {year}
+          </h3>
+          <div className="Hours">
+            <p>Hours</p>
+            <h1>{hour}</h1>
+          </div>
+          <p className="Colon_1">:</p>
+          <div className="Mins">
+            <p>Mins</p>
+            <h1>{min}</h1>
+          </div>
+          <p className="Colon_2">:</p>
+          <div className="Secs">
+            <p>Secs</p>
+            <h1>{sec}</h1>
+          </div>
+        </div>
+      </div>
     );
+  }
+
+  return (
+    <div>
+      <div className="header NewsPage_Header">
+        <div className="header-top" />
+        <div className="NavBar">
+          <a className="Company_Logo"><img src="https://res.cloudinary.com/ddsomtotk/image/upload/v1618887646/57dd63e9c36d40e8aa369502ee886d0e_lmpcru.png" alt="Comp_logo" /></a>
+        </div>
+        <div className="header-bottom" />
+        <center>
+          <div className="dash_card">
+            <ul className="dash_btn">
+              <img className="profile_img" src="https://res.cloudinary.com/ddsomtotk/image/upload/v1618947076/Prof_img_kxovem.png" />
+              <button data-testid="profile-btn" onClick={onClickProfile} className="prof-btn btn">Personal Profile</button>
+              <img className="weather_img" src="https://res.cloudinary.com/ddsomtotk/image/upload/v1618946882/WeatherImg_d05jmk.png" />
+              <button data-testid="weather-btn" onClick={onClickWeather} className="weath-btn btn">Local Weather</button>
+              <img className="news_img" src="https://res.cloudinary.com/ddsomtotk/image/upload/v1618946888/Newsimg_harljy.png" />
+              <button data-testid="news-btn" onClick={onClickNews} className="news-btn btn">News Updates</button>
+              <img className="todo_img" src="https://res.cloudinary.com/ddsomtotk/image/upload/v1618946885/Todolist_su8dg0.png" />
+              <button data-testid="todo-btn" onClick={onClickTodo} className="todo-btn btn">Daily Tasks</button>
+            </ul>
+          </div>
+        </center>
+      </div>
+      {message}
+      <div className="Comp_Render">
+        {test}
+      </div>
+      <div className="footer NewsPage_Footer">
+        <div className="footer-top" />
+        <div className="footer-center NewsPage_FooterText"><p>&copy; SASA Inc. All Rights Reserved.</p></div>
+        <div className="footer-bottom" />
+      </div>
+    </div>
+  );
 }
 
 export default DashBoard;
