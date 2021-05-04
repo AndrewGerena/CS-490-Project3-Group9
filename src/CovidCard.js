08c777ec08db02f686e6da3dbb1cdfcea143b533
Andrew Gerena <amg46@njit.edu>
2021-05-04 03:53:29 +0000

    Linted CovidCard

--- src/CovidCard.js
+++ src/CovidCard.js
@@ -4,26 +4,26 @@ import React from 'react';
 
 export function CovidCard(props) {
   return (
-    <div className="Covid_Card" ><span><u>Results As Of:</u></span>&nbsp;
-       <i className="Date">{props.Covid_Date}</i>
-      <br/>
+    <div className="Covid_Card"><span><u>Results As Of:</u></span>&nbsp;
+      <i className="Date">{props.Covid_Date}</i>
+      <br />
       <span><u>Cases:</u></span>&nbsp;
-       <i className="TotalCases">{props.Covid_TotalCases}</i>
-      <br/>
+      <i className="TotalCases">{props.Covid_TotalCases}</i>
+      <br />
       <span><u>News Cases:</u></span>&nbsp;
-       <i className="NewCases">{props.Covid_NewCases}</i>
-      <br/>
-      <span><u>Deaths:</u></span>&nbsp; 
-       <i className="TotalDeaths">{props.Covid_TotalDeaths}</i>
-      <br/>
+      <i className="NewCases">{props.Covid_NewCases}</i>
+      <br />
+      <span><u>Deaths:</u></span>&nbsp;
+      <i className="TotalDeaths">{props.Covid_TotalDeaths}</i>
+      <br />
       <span><u>New Deaths:</u></span>&nbsp;
-       <i className="NewDeaths">{props.Covid_NewDeaths}</i>
-      <br/>
+      <i className="NewDeaths">{props.Covid_NewDeaths}</i>
+      <br />
       <span><u>Recovered:</u></span>&nbsp;
-       <i className="TotalRecovered">{props.Covid_TotalRecovered}</i>
-      <br/>
+      <i className="TotalRecovered">{props.Covid_TotalRecovered}</i>
+      <br />
       <span><u>New Recovered:</u></span>&nbsp;
-       <i className="NewRecovered">{props.Covid_NewRecovered}</i>
+      <i className="NewRecovered">{props.Covid_NewRecovered}</i>
     </div>
   );
 }
